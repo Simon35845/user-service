ebda52974224aaa456feec53e9b997a13c852202
Admin5195 <DrakeKingman1991@gmail.com>
2026-09-02 11:50:23 +0300

    hibernate config

---
+++ src/main/java/com/example/HibernateUtil.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration()
+                    .configure("hibernate.cfg.xml");
+
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+}
