177543f0121741264c89bf671761c4ed2984888b
Admin5195 <DrakeKingman1991@gmail.com>
2026-09-06 15:02:25 +0300

    Переписал UserValidator: на каждое поле сделан отдельный метод валидации. Вместо логов сделан вывод в консоль (логи оставил только для сервиса).

--- src/main/java/com/example/UserValidator.java
+++ src/main/java/com/example/UserValidator.java
@@ -1,34 +1,46 @@
 package com.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class UserValidator {
-    private static final Logger log = LoggerFactory.getLogger(UserValidator.class);
 
-    public static boolean validateUserData(String name, String email, Integer age) {
-        if (name == null || name.isBlank()) {
-            log.warn("Имя не может быть пустым");
+    public boolean isIdValid(Integer id) {
+        if (id == null) {
+            System.out.println("id не может быть пустым.");
             return false;
         }
+        if (id <= 0) {
+            System.out.println("Id должен быть больше 0.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isNameValid(String name) {
+        if (name == null || name.isBlank()) {
+            System.out.println("Имя не может быть пустым.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isEmailValid(String email) {
         if (email == null || email.isBlank()) {
-            log.warn("Email не может быть пустым");
+            System.out.println("Email не может быть пустым.");
             return false;
         }
         if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")) {
-            log.warn("Некорректный формат email: {}", email);
+            System.out.println("Некорректный формат email.");
             return false;
         }
+        return true;
+    }
+
+    public boolean isAgeValid(Integer age) {
         if (age == null) {
-            log.warn("Возраст не может быть пустым");
+            System.out.println("Возраст не может быть пустым.");
             return false;
         }
-        if (age < 0) {
-            log.warn("Возраст не может быть отрицательным: {}", age);
-            return false;
-        }
-        if (age > 150) {
-            log.warn("Возраст не может превышать 150 лет: {}", age);
+        if (age < 0 || age > 200) {
+            System.out.println("Возраст не может быть меньше 0 или больше 200 лет.");
             return false;
         }
         return true;
